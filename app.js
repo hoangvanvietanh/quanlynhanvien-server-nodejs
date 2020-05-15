5af966f607efcef2be23bd8452befffc509592e7
Viet Anh Hoang Van <hoangvanvietanh@gmail.com>
2020-05-15 10:13:59 +0700

    test bot vs dich vu ^^

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 var NodeJs_Dich_vu = require("http")
 var Luu_tru = require("./Xu_ly/XL_LUU_TRU")
 var Port = normalizePort(process.env.PORT || 1200);
@@ -5,6 +6,14 @@ var Xu_ly_Tham_so = require('querystring')
 var Du_lieu = {}
 var Danh_sach_Nhan_vien = Luu_tru.Doc_Danh_sach_Nhan_vien()
 var Doc_Danh_sach_Nhan_vien_Tu_MMSql = Luu_tru.Doc_Danh_sach_Nhan_vien_Tu_MSSql()
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
+var PAGE_ACCESS_TOKEN = "EAAJumKeUliYBAIS1IM8zDPbuy7ldiKrNWHF2s34ZAH51vTkAL5AwSyMQXbTJNSplzrCSXKSVWlWvZATOax9w6AZCNnGjUIEUxxlsZCrErCzpXrU58GewA4auReTpNvYv1teC9hEt8dXIafqvt3Foiwajhn2ZAes333BMCisTuQM7193ciZAytk7WChDkJRySIZD";
+var VERIFY_TOKEN = "vietanh";
+var listUser = ["3119611768102210","2825696464152069"];
 
 Danh_sach_Nhan_vien.then(Kq => {
     Du_lieu.Danh_sach_Nhan_vien = Kq;
@@ -13,6 +22,81 @@ Danh_sach_Nhan_vien.then(Kq => {
 // Doc_Danh_sach_Nhan_vien_Tu_MMSql.then(Kq => {
 //     Du_lieu.Doc_Danh_sach_Nhan_vien_Tu_MMSql = Kq;
 // })
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Webhook validation
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }
+});
+
+// Display the web page
+app.get('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(messengerButton);
+  res.end();
+});
+
+// Display the web page
+app.get('/apply', function(req, res) {
+    console.log("okey apply");
+  for(var i = 0; i<100;i++)
+  {
+    listUser.forEach(x=>{
+      sendTextMessage(x, "Xin chào nhóc ác :) "+ i);
+    })
+  }
+  
+  res.write(JSON.stringify(listUser));
+  res.end();
+});
+
+// Message processing
+app.post('/webhook', function (req, res) {
+  console.log(req.body);
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else if (event.postback) {
+          receivedPostback(event);   
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
 
 var Dich_vu = NodeJs_Dich_vu.createServer((Yeu_cau, Dap_ung) => {
     var Chuoi_Nhan = ""
@@ -123,7 +207,7 @@ Dich_vu.listen(Port,
     
     console.log(`Dịch vụ Dữ liệu đang thực thi tại địa chỉ: http://localhost:${Port}`)
 );
-
+app.listen(1201,console.log(`Dịch vụ Bot thi tại địa chỉ: http://localhost:${1201}`));
 Dich_vu.on('error', onError);
 Dich_vu.on('listening', onListening);
 
@@ -186,3 +270,151 @@ function onListening() {
         'port ' + addr.port;
     console.log('Listening on ' + bind);
 }
+
+
+// Incoming events handling
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+  
+    console.log("Received message for user %d and page %d at %d with message:", 
+      senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+  
+    var messageId = message.mid;
+  
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+  
+    if (messageText) {
+      // If we receive a text message, check to see if it matches a keyword
+      // and send back the template example. Otherwise, just echo the text we received.
+      var mess = messageText.split("-")
+      console.log(mess)
+      if(mess[0] == "apply" && mess.length == 2)
+      {
+        messageText = "apply";
+      }
+      
+      switch (messageText) {
+        case 'generic':
+          sendGenericMessage(senderID);
+          break;  
+        case 'apply' :
+          sendTextMessage(senderID, "Đăng ký thành công nè :) ");
+          listUser.push(senderID);
+          break;
+        case 'hi':
+          sendTextMessage(senderID, "Chào bạn :) ");
+          break;
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+    } else if (messageAttachments) {
+      sendTextMessage(senderID, "Message with attachment received");
+    }
+  }
+  
+  function receivedPostback(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+  
+    // The 'payload' param is a developer-defined field which is set in a postback 
+    // button for Structured Messages. 
+    var payload = event.postback.payload;
+  
+    console.log("Received postback for user %d and page %d with payload '%s' " + 
+      "at %d", senderID, recipientID, payload, timeOfPostback);
+  
+    // When a postback is called, we'll send a message back to the sender to 
+    // let them know it was successful
+    sendTextMessage(senderID, "Postback called");
+  }
+  
+  //////////////////////////
+  // Sending helpers
+  //////////////////////////
+  function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: messageText
+      }
+    };
+  
+    callSendAPI(messageData);
+  }
+  
+  function sendGenericMessage(recipientId) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [{
+              title: "rift",
+              subtitle: "Next-generation virtual reality",
+              item_url: "https://www.oculus.com/en-us/rift/",               
+              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/rift/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for first bubble",
+              }],
+            }, {
+              title: "touch",
+              subtitle: "Your Hands, Now in VR",
+              item_url: "https://www.oculus.com/en-us/touch/",               
+              image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/touch/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for second bubble",
+              }]
+            }]
+          }
+        }
+      }
+    };  
+  
+    callSendAPI(messageData);
+  }
+  
+  function callSendAPI(messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: messageData
+  
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+  
+        console.log("Successfully sent generic message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+      }
+    });  
+  }
